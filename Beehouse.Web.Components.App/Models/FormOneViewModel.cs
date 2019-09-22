3ee5473c3812f7635d8b4a62eb6a92d2f2ff9b23
medeirosraul <32266934+medeirosraul@users.noreply.github.com>
2019-09-22 20:46:22 -0300

    Commit

---
+++ Beehouse.Web.Components.App/Models/FormOneViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beehouse.Web.Components.App.Models
+{
+    public class FormOneViewModel
+    {
+        public string InputText { get; set; }
+        public string InputNumber { get; set; }
+    }
+}
